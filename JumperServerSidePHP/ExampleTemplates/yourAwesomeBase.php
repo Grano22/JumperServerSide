88c7a6e95d1967456b03413b679d925d0ecee0ad
Grano22 <grano22@outlook.com>
2021-01-30 01:46:10 +0100

    Updated description of usage

--- JumperServerSidePHP/ExampleTemplates/yourAwesomeBase.php
+++ JumperServerSidePHP/ExampleTemplates/yourAwesomeBase.php
@@ -2,3 +2,10 @@
 <html lang="$_TEMPLATE['lang']">
   <excluded replaceInto="body">Failed to load template..</excluded>
 </html>
+<?php
+/*
+excluded - a special template elements, that allows extending templates
+replaceInto attribute means replace <excluded> tag into body when excluding template fails
+In excluded element you can specify alternative HTML code if replacing this element fails
+*/
+?>
